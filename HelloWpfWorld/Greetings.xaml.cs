e7e1293cba111dad89215276f69a623c5c264449
PaulTouchtoin <p_touchton@yahoo.com>
2019-10-14 07:38:44 -0700

    Add project files.

---
+++ HelloWpfWorld/Greetings.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System.Windows;
+
+namespace HelloWpfWorld
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (HelloButton.IsChecked == true)
+            {
+                MessageBox.Show("Hello.");
+            }
+            else if (GoodbyeButton.IsChecked == true)
+            {
+                MessageBox.Show("Goodbye.");
+            }
+        }
+    }
+}
